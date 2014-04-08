8195e4252d917c9a64caf8dce6ede7bc79f6f1b8
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-08 17:40:21 -0400

    Added Validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,7 @@ describe Comment do
   it { should belong_to :link }
   it { should have_one(:user), through: :link }
 
+  it {should validate_presence_of :content}
+  it {should validate_presence_of :user_id}
+
 end
