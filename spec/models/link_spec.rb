68255e9f4894fdc7a380e32345af56e8ab0bba89
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-08 17:39:29 -0400

    URLs must be unique also added validations

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,8 @@ describe Link do
   it { should have_many :votes }
   it { should belong_to :user }
 
+  it { should validate_uniqueness_of :url }
+  it { should validate_presence_of :url }
+  it { should validate_presence_of :title }
+
 end
