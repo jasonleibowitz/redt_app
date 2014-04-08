4983d42f77cc279e4889326bc6802ec6145f9a10
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-08 17:39:01 -0400

    Users can submit links and view all links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,32 @@
 class LinksController < ApplicationController
 
+  before_action :require_authentication, only: [:index, :show, :update, :destroy]
+
   def index
-    @links = Link.all
+    @links = Link.all.order(created_at: :desc)
   end
 
   def show
     @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    @link.user = current_user
+    if @link.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def link_params
+    return params.require(:link).permit(:title, :url)
   end
 
 
