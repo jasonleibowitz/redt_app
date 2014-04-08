1df07087959ee61356f20cec3e082b852f7df0ce
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-08 17:39:39 -0400

    Added validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,13 @@ describe User do
 
   it {should have_secure_password}
 
+  it {should validate_presence_of :first_name}
+  it {should validate_presence_of :last_name}
+  it {should validate_presence_of :email}
+  it {should validate_presence_of :password}
+  it {should validate_presence_of :password_confirmation}
+  it {should ensure_length_of(:password).is_at_least(5)}
+
+  it {should validate_confirmation_of :password}
+
 end
